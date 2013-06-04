b2fba52e8f00fee6a3e2cf614c08eb757eeab3cf
Rob Golding <rob@robgolding.com>
2013-06-04 20:12:12 +0100

    Add link script

---
+++ link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s .gitconfig ~/
+ln -s .git.scmbrc ~/
+ln -s .gvimrc ~/
+ln -s .vimrc ~/
+ln -s .redshift.conf ~/
+ln -s .taskrc ~/
+ln -s .tmux.conf ~/
+ln -s .tmux.number.sh ~/
